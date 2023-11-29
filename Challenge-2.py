4ed3090fd8899be7e0509de220ae874128c9e9dd
UHasanU <mualimha+1@gmail.com>
2023-11-29 20:48:01 +0300

    Something

---
+++ Challenge-2.py
@@ -0,0 +1,14 @@
+def main():
+    number = RandomNumber()
+    guess = input("Enter a number")
+    CheckUserGuess(number, guess)
+    return None
+
+
+def RandomRandom():
+    return None
+
+def CheckUserChoice():
+    return None
+
+main()
