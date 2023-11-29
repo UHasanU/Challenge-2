36105bde9a01cc36f0b559e74baeebf2d6743736
UHasanU <mualimha+1@gmail.com>
2023-11-29 20:59:45 +0300

    added logic to main and check number

--- Challenge-2.py
+++ Challenge-2.py
@@ -1,14 +1,21 @@
 def main():
     number = RandomNumber()
     guess = input("Enter a number")
-    CheckUserGuess(number, guess)
-    return None
+    print(CheckUserGuess(number, guess))
 
 
 def RandomRandom():
     return None
 
-def CheckUserChoice():
-    return None
+def CheckUserChoice(number, guess):
+    if guess = number:
+        return true
+    elif -1 <= number-guess <= 1:
+        return "Very close"
+    elif -2 <= number-guess <= 2
+        return "Close"
+    else:
+        return "Wrong"
+    
 
 main()
